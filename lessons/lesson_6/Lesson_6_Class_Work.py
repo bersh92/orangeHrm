320121d0b539e13ee85aac788183a7540c6cead6
aleksbrain91 <oleksandr25popov@gmail.com>
2024-03-19 17:25:48 -0500

    Little bit confused with Lesson_6_Class_Work.py I wrote some comments inside the file

--- lessons/lesson_6/Lesson_6_Class_Work.py
+++ lessons/lesson_6/Lesson_6_Class_Work.py
@@ -16,3 +16,40 @@ Steps to Complete the Assignment:
 6. Use a loop to iterate through these sections and collect their names or any identifiable text.
 7. Print the names of all sections found under the 'More' dropdown to the console.
 """
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://portnov_admin-trials711.orangehrmlive.com/client/#/dashboard")
+
+driver.find_element(By.CSS_SELECTOR, "input[id='txtUsername']").send_keys("Admin")
+driver.find_element(By.CSS_SELECTOR, "input[id='txtPassword']").send_keys("qTJn5@5APu")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+time.sleep(5)
+
+sidebar_toggle = driver.find_element(By.CSS_SELECTOR, '#sidebar-toggle[class="circle"]')
+sidebar_menu = driver.find_element(By.CSS_SELECTOR, '#left-menu[class="sidebar menu-visible"]')
+if not sidebar_menu.is_displayed():
+    sidebar_toggle.click()
+    time.sleep(1)
+    # driver.find_element(By.XPATH, '//a/span[text()="HR Administration"][1]').click()
+    # I suppose you meant to click on 'Employee Management' because 'HR Administration' doesn't have 'More' section
+    driver.find_element(By.XPATH, '//a/span[text()="Employee Management"][1]').click()
+else:
+    # driver.find_element(By.XPATH, '//a/span[text()="HR Administration"][1]').click()
+    driver.find_element(By.XPATH, '//a/span[text()="Employee Management"][1]').click()
+time.sleep(3)
+
+driver.find_element(By.XPATH, '//a[text()="More "]').click()
+more_dropdown = driver.find_elements(By.XPATH, "//a[contains(text(),'More ')]/following-sibling::sub-menu-container//a")
+more_dropdown_names = []
+for name in more_dropdown:
+    more_dropdown_names.append(name.text)
+print(more_dropdown_names)
+#I give up
+#I tried using XPath and CSS to find only the element from the 'More' section dropdown.
+# However, when searching for them in the DevTools on a webpage,
+# it found 16 elements that are truly related to the 'More' section. But PyCharm prints unexpected text and also text not from More section
+driver.quit()
--- lessons/lesson_6/Lesson_6_Home_Work_conditions.py
+++ lessons/lesson_6/Lesson_6_Home_Work_conditions.py
@@ -12,3 +12,37 @@ Locate the user status option on the "Add User" form.
 Check the current status of the user. If it is not "Disabled," you need to select the "Disabled" option.
 Ensure your script can handle both conditions: if the status is already "Disabled," it remains unchanged; otherwise, change it to "Disabled."
 '''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://portnov_admin-trials711.orangehrmlive.com/client/#/dashboard")
+
+driver.find_element(By.CSS_SELECTOR, "input[id='txtUsername']").send_keys("Admin")
+driver.find_element(By.CSS_SELECTOR, "input[id='txtPassword']").send_keys("qTJn5@5APu")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+time.sleep(5)
+
+sidebar_toggle = driver.find_element(By.CSS_SELECTOR, '#sidebar-toggle[class="circle"]')
+sidebar_menu = driver.find_element(By.CSS_SELECTOR, '#left-menu[class="sidebar menu-visible"]')
+if not sidebar_menu.is_displayed():
+    sidebar_toggle.click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//a/span[text()="HR Administration"][1]').click()
+else:
+    driver.find_element(By.XPATH, '//a/span[text()="HR Administration"][1]').click()
+time.sleep(3)
+
+driver.find_element(By.XPATH, '//i[text()="add"]').click()
+time.sleep(2)
+
+enabled_button = driver.find_element(By.XPATH, '//label[text()="Enabled"]')
+disabled_button = driver.find_element(By.XPATH, '//label[text()="Disabled"]')
+if enabled_button.is_selected():
+    disabled_button.click()
+    time.sleep(2)
+elif disabled_button.is_selected():
+    time.sleep(2)
+driver.quit()
