1b4043f41681dda2c60c32a86d6082a07fe7f814
liliiasattarova <sat-lilya@yandex.ru>
2024-03-20 13:10:35 -0400

    Complete home work

---
+++ lessons/lesson_5/test.py
@@ -0,0 +1,5 @@
+number = 6
+if number > 10:
+    print(number, 'is greater than 10')
+else:
+    print(number, 'is less than 10')
--- lessons/lesson_6/Lesson_6_Home_Work_loops.py
+++ lessons/lesson_6/Lesson_6_Home_Work_loops.py
@@ -12,3 +12,28 @@ On the "Add User" form, identify all input fields related to user information.
 Use a loop to iterate over these input fields and collect their names.
 Print the list of field names you have collected.
 '''
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://portnov_admin-trials711.orangehrmlive.com/client/#/dashboard")
+
+driver.find_element(By.CSS_SELECTOR, "input[id='txtUsername']").send_keys("Admin")
+driver.find_element(By.CSS_SELECTOR, "input[id='txtPassword']").send_keys("qTJn5@5APu")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+time.sleep(5)
+section_texts = []
+driver.find_element(By.XPATH, '(//div[@id="menu-container"] //span[text()="HR Administration"])[1]').click()
+
+time.sleep(5)
+section_texts = []
+driver.find_elements(By.CSS_SELECTOR, 'a[class="btn-floating btn-large waves-effect waves-light"]').click()
+
+time.sleep(5)
+section_texts = []
+menu_items = driver.find_elements(By.CSS_SELECTOR, 'input-field')
+for field in menu_items:
+    section_texts.append(field.text)
