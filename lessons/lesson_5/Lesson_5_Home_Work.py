3f026c001d1d19f15413aac63cd19695a2d8070f
isukhova <ibardaeva81@gmail.com>
2024-03-22 20:15:31 +0300

    finish the home work 5

--- lessons/lesson_5/Lesson_5_Home_Work.py
+++ lessons/lesson_5/Lesson_5_Home_Work.py
@@ -22,7 +22,7 @@ def sum_two_numbers():
 
 
 def greet_user():
-    """Task 4: Use the input() function to ask the user for their name and greet them."""
+    """Task 4: Use the input() function to ask the user for their name and greet them.пнглгш"""
     print('\ngreet_user():')
     def greet(name):
         return "Hello, " + name + "!"
