1cdc50e68d09b15b445990947cb324f139a4b81a
isukhova <ibardaeva81@gmail.com>
2024-03-24 19:10:21 +0300

    finish the home work 6

--- lessons/lesson_6/Lesson_6_Home_Work_conditions.py
+++ lessons/lesson_6/Lesson_6_Home_Work_conditions.py
@@ -12,3 +12,28 @@ Locate the user status option on the "Add User" form.
 Check the current status of the user. If it is not "Disabled," you need to select the "Disabled" option.
 Ensure your script can handle both conditions: if the status is already "Disabled," it remains unchanged; otherwise, change it to "Disabled."
 '''
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://portnov_admin-trials711.orangehrmlive.com/client/#/dashboard")
+
+driver.find_element(By.CSS_SELECTOR, "input[id='txtUsername']").send_keys("Admin")
+driver.find_element(By.CSS_SELECTOR, "input[id='txtPassword']").send_keys("qTJn5@5APu")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR, 'div[class="dashboard-widget-config-button"]').click()
+time.sleep(5)
+buttons = driver.find_elements(By.CSS_SELECTOR, 'div[class="configuration-tabs"] input[id*="adminWidgetSwitch"]')
+
+for item in buttons:
+    attribute_value = item.get_attribute('class')
+    if 'ng-not-empty' in attribute_value:
+        parent_div = item.find_element(By.XPATH, "..")
+        parent_div.click()
+
+
+driver.quit()
--- lessons/lesson_6/Lesson_6_Home_Work_loops.py
+++ lessons/lesson_6/Lesson_6_Home_Work_loops.py
@@ -12,3 +12,31 @@ On the "Add User" form, identify all input fields related to user information.
 Use a loop to iterate over these input fields and collect their names.
 Print the list of field names you have collected.
 '''
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://portnov_admin-trials711.orangehrmlive.com/client/#/dashboard")
+
+driver.find_element(By.CSS_SELECTOR, "input[id='txtUsername']").send_keys("Admin")
+driver.find_element(By.CSS_SELECTOR, "input[id='txtPassword']").send_keys("qTJn5@5APu")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+time.sleep(5)
+driver.find_element(By.XPATH, "(//div[@id='menu-container'] //span[text()='HR Administration'])[1]").click()
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR, 'div[class="fixed-action-btn floating-add-btn tooltipped"]').click()
+
+time.sleep(5)
+section_texts = []
+menu_items = driver.find_elements(By.XPATH, "//div[@class='modal-content ']//label")
+
+for item in menu_items:
+    section_texts.append(item.text)
+
+print(section_texts)
+
+# Close the browser
+driver.quit()
